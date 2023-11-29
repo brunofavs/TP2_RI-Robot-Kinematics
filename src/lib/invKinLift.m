06c24f65fc2b6f9b1c20ef501c45510db2ca6622
brunofavs <bruno.favs@ua.pt>
2023-11-29 15:31:43 +0000

    Lift incorporated with better redundancy

--- src/lib/invKinLift.m
+++ src/lib/invKinLift.m
@@ -2,7 +2,7 @@ function Q = invKinLift(x16, y16, z,La,Lb, Lc, Ld)
 
     q2 = 2*atan((2*Lb*y16 - (-(La^2 + 2*La*Lc + 2*La*y16 - Lb^2 - 2*Lb*Ld + Lc^2 + 2*Lc*y16 - Ld^2 + x16^2 + y16^2)*(La^2 + 2*La*Lc + 2*La*y16 - Lb^2 + 2*Lb*Ld + Lc^2 + 2*Lc*y16 - Ld^2 + x16^2 + y16^2))^(1/2) + 2*La*Lb + 2*Lb*Lc)/(La^2 + 2*La*Lc + 2*La*y16 + Lb^2 + 2*Lb*x16 + Lc^2 + 2*Lc*y16 - Ld^2 + x16^2 + y16^2)) + 2*pi*0;
 
-    q4 = 2*pi*1 - 2*atan((2*Ld*y16 - (-(La^2 + 2*La*Lc + 2*La*y16 - Lb^2 - 2*Lb*Ld + Lc^2 + 2*Lc*y16 - Ld^2 + x16^2 + y16^2)*(La^2 + 2*La*Lc + 2*La*y16 - Lb^2 + 2*Lb*Ld + Lc^2 + 2*Lc*y16 - Ld^2 + x16^2 + y16^2))^(1/2) + 2*La*Ld + 2*Lc*Ld)/(La^2 + 2*La*Lc + 2*La*y16 - Lb^2 + Lc^2 + 2*Lc*y16 + Ld^2 - 2*Ld*x16 + x16^2 + y16^2));
+    q4 = 2*pi*0 - 2*atan((2*Ld*y16 - (-(La^2 + 2*La*Lc + 2*La*y16 - Lb^2 - 2*Lb*Ld + Lc^2 + 2*Lc*y16 - Ld^2 + x16^2 + y16^2)*(La^2 + 2*La*Lc + 2*La*y16 - Lb^2 + 2*Lb*Ld + Lc^2 + 2*Lc*y16 - Ld^2 + x16^2 + y16^2))^(1/2) + 2*La*Ld + 2*Lc*Ld)/(La^2 + 2*La*Lc + 2*La*y16 - Lb^2 + Lc^2 + 2*Lc*y16 + Ld^2 - 2*Ld*x16 + x16^2 + y16^2));
 
     q3 = -q2;
 
